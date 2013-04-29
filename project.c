7663955290619aa2b96504552d5f85ce4b56e5b7
JohnMave126 <johnmave126@gmail.com>
2013-04-30 00:49:10 +0800

    test

--- project.c
+++ project.c
@@ -8,45 +8,56 @@ inline int max(a, b){
     return (a>b)?(a):(b);
 }
 
-struct test {
-    int x, y;
-};
 
 void create_simple_bomb(int id, int x_loc, int y_loc, int speed) {
     __asm__(
         "li     $v0, 106\n\t"
-        "syscall\n\t":::"v0");
+        "syscall\n\t");
 }
 
 void create_remote_bomb(int id, int x_loc, int y_loc, int speed) {
     __asm__(
         "li     $v0, 107\n\t"
-        "syscall\n\t":::"v0");
+        "syscall\n\t");
 }
 
 int getX(int id) {
-    int x;
     __asm__(
-        "move   $a0, %1\n\t"
         "li     $v0, 110\n\t"
-        "syscall\n\t"
-        "move   %0, $v0\n\t":"=r"(x):"r"(id):"a0");
-    return x;
+        "syscall\n\t");
 }
 
 int getY(int id) {
-    int x;
     __asm__(
-        "move   $a0, %1\n\t"
         "li     $v0, 110\n\t"
         "syscall\n\t"
-        "move   %0, $v1\n\t":"=r"(x):"r"(id):"a0");
-    return x;
+        "move   $v0, $v1\n\t");
+}
+
+void activate_r_bomb(int id) {
+    __asm__(
+        "li     $v0, 109\n\t"
+        "syscall\n\t");
 }
 
 void emit_one_bomb() {
     if(bomb_count < bomb_num) {
-        
+        create_simple_bomb(bomb_ids[bomb_count], getX(1), getY(1), 6);
+        bomb_count++;
+    }
+}
+
+void emit_one_rbomb() {
+    if(rbomb_count < rbomb_num) {
+        create_remote_bomb(rbomb_ids[rbomb_count], getX(1), getY(1), 6);
+        rbomb_count++;
+    }
+}
+
+void activate_rbombs(){
+    int i;
+    for(i = 0; i < rbomb_count; i++) {
+        activate_r_bomb(rbomb_ids[i]);
     }
 }
 
@@ -60,9 +71,6 @@ int check_intersection(int *rec1, int *rec2) {
     return 1;
 }
 
-struct test pp() {
-    struct test t;
-    t.x = 19;
-    t.y = 20;
-    return t;
+void pp() {
+    create_remote_bomb(10, 12,23,2);
 }
